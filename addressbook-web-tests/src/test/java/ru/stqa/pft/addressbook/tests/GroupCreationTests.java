c3e4ff0cab683a943d3dd1dadb41f80955834865
Mikalai.Skrabets <mikalai.skrabets@adastragrp.com>
2019-04-22 17:18:03 +0200

    Lection 3.10. Checking and realizing preconditions.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,11 +9,7 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() {
 
     app.getNavigationHelper().goToGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test", "test", null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
-
+    app.getGroupHelper().createGroup(new GroupData("test", "test", null));
   }
 
 }
