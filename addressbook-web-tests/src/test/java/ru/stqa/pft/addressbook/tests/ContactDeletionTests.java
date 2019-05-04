3d9971c820c2d48473cadc7c5d57d70994883251
Mikalai.Skrabets <mikalai.skrabets@adastragrp.com>
2019-05-04 12:17:21 +0200

    Add @BeforeMethods to contact modification and deletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,11 +9,15 @@ import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
 
-  @Test
-  public void testContactDeletion() {
+  @BeforeMethod
+  public void ensurePreConditions() {
     if (!app.contact().isThereAContact()) {
       app.contact().createContact(new ContactData("Petr", "Jan", "Mares", "[none]", "petrmares", "PHDr", "Skoda", "Prague", "petrmares@test.com"), true);
     }
+  }
+
+  @Test
+  public void testContactDeletion() {
     List<ContactData> before = app.contact().list();
     app.contact().selectContact(before.size() - 1);
     app.contact().deleteSelectedContacts();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -9,20 +10,23 @@ import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
-  @Test
-  public void testContactModification() {
+  @BeforeMethod
+  public void ensurePreConditions() {
     if (!app.contact().isThereAContact()) {
       app.contact().createContact(new ContactData("Petr", "Jan", "Mares", "[none]", "petrmares", "PHDr", "Skoda", "Prague", "petrmares@test.com"), true);
     }
+  }
+
+  @Test
+  public void testContactModification() {
     List<ContactData> before = app.contact().list();
-    app.contact().initContactModification(before.size() - 1);
-    ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "test", "test");
-    app.contact().fillContactForm(contact, false);
-    app.contact().submitContactModification();
+    int index = before.size() - 1;
+    ContactData contact = new ContactData(before.get(index).getId(), "test", "test");
+    app.contact().modifyContact(index, contact);
     List<ContactData> after = app.contact().list();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(before.size() - 1);
+    before.remove(index);
     before.add(contact);
     Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
     before.sort(byId);
