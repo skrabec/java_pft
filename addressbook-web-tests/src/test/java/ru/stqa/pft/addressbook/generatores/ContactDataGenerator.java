7bbab2ac5fc2fa86a0ed7b3b2c6ab5db1bfef152
Mikalai.Skrabets <mikalai.skrabets@adastragrp.com>
2019-05-14 14:55:04 +0200

    Task 12. Contact data generator

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generatores/ContactDataGenerator.java
@@ -0,0 +1,68 @@
+package ru.stqa.pft.addressbook.generatores;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (
+            ParameterException e) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    if (format.equals("json")) {
+      saveAsJSON(contacts, new File(file));
+    } else {
+      System.out.println("Unrecognized format " + format);
+    }
+  }
+
+  private void saveAsJSON(List<ContactData> contacts, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
+  }
+
+  private List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData().withFirstName(String.format("firstname %s", i)).withLastName(String.format("lastname %s", i)).withMiddleName(String.format("middlename %s", i))
+              .withGroup("[none]").withNickName(String.format("nickname %s", i)).withTitle(String.format("title %s", i))
+              .withCompany(String.format("company %s", i)).withAddress(String.format("address %s", i)).withEmail(String.format("test%s@mail.com", i)).withMobile(String.format("%s", i))
+              .withHomePhone(String.format("%s", i)).withWorkPhone(String.format("%s", i)).withEmail2(String.format("test%s@mail.com", i)));
+    }
+    return contacts;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,24 +1,41 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+
 import java.io.File;
 
 public class ContactData {
 
   private int id = Integer.MAX_VALUE;
+  @Expose
   private String firstName;
+  @Expose
   private String middleName;
+  @Expose
   private String lastName;
+  @Expose
   private String group;
+  @Expose
   private String nickName;
+  @Expose
   private String title;
+  @Expose
   private String company;
+  @Expose
   private String address;
+  @Expose
   private String email;
+  @Expose
   private String email2;
+  @Expose
   private String email3;
+  @Expose
   private String homePhone;
+  @Expose
   private String mobile;
+  @Expose
   private String workPhone;
+  @Expose
   private String allPhones;
   private String allEmails;
   private File photo;
