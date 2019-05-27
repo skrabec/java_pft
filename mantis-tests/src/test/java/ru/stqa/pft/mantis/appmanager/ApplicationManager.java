c6e6497d8898600c4ec6b854651d7c346cc2b2b0
Mikalai.Skrabets <mikalai.skrabets@adastragrp.com>
2019-05-27 14:23:09 +0200

    Lection 8.2. New test module for Mantis is added

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+
+  private final Properties properties;
+  private String browser;
+  WebDriver wd;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+    }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    }
+
+    wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.BaseUrl"));
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+}
