544d37ba178fe8c2f1c4341119e8a2a61f075756
Mikalai.Skrabets <mikalai.skrabets@adastragrp.com>
2019-06-01 17:38:39 +0200

    Task 18. Test passed with built-in mail server.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTest.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTest.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.Model.MailMessage;
@@ -7,29 +10,40 @@ import ru.stqa.pft.mantis.Model.UserData;
 import ru.stqa.pft.mantis.Model.Users;
 
 import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.List;
 
 public class ChangePasswordTest extends TestBase {
 
+  @BeforeMethod
+  public void startMailServer() {
+    app.mail().start();
+  }
+
   @Test
-  public void testChangePassword() throws MessagingException {
-    String password = "password";
-    String newPassword = "1234";
+  public void testChangePassword() throws MessagingException, IOException {
+    String newPassword = "5555";
     String admin = "Administrator";
     String adminpass = "root";
     Users users = app.db().users();
     UserData user = users.iterator().next();
     app.login().start(admin, adminpass);
     app.manageUsers().resetPassword(user.getUsername());
-    List<MailMessage> mailMessages = app.james().waitForMail(user.getUsername(), password, 60000);
+    List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000);
+    //List<MailMessage> mailMessages = app.james().waitForMail(user.getUsername(), password, 60000);
     String confirmationLink = findConfirmationLink(mailMessages, user.getEmail());
-    //app.manageUsers().applyNewPassword(confirmationLink, newPassword);
-
+    app.manageUsers().applyNewPassword(confirmationLink, newPassword);
+    Assert.assertTrue(app.newSession().login(user.getUsername(), newPassword));
   }
 
   private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
     MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
     VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
     return regex.getText(mailMessage.text);
+  }
+
+  @AfterMethod
+  public void stopMailServer() {
+    app.mail().stop();
   }
 }
